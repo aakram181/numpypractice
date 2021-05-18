7d2aeaf766ef2ee516e2a4f8ffc5392b969bd0d7
aakram181 <68343706+aakram181@users.noreply.github.com>
2021-05-18 11:12:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+def randomization(n):
+    """
+    Arg:
+      n - an integer
+    Returns:
+      A - a randomly-generated nx1 Numpy array.
+    """
+    array = np.random.rand(1,n)
+    return array
+    raise NotImplementedError
+def operations(h, w):
+    """
+    Takes two inputs, h and w, and makes two Numpy arrays A and B of size
+    h x w, and returns A, B, and s, the sum of A and B.
+
+    Arg:
+      h - an integer describing the height of A and B
+      w - an integer describing the width of A and B
+    Returns (in this order):
+      A - a randomly-generated h x w Numpy array.
+      B - a randomly-generated h x w Numpy array.
+      s - the sum of A and B.
+    """
+    a = np.random.rand(h,w)
+    b = np.random.rand(h,w)
+    sum = np.add(a,b)
+    return a,b,sum
+    raise NotImplementedError
+
+
+def norm(A, B):
+    """
+    Takes two Numpy column arrays, A and B, and returns the L2 norm of their
+    sum.
+
+    Arg:
+      A - a Numpy array
+      B - a Numpy array
+    Returns:
+      s - the L2 norm of A+B.
+    """
+    return np.linalg.norm(np.add(A,B))
+    raise NotImplementedError
+
+
+def neural_network(inputs, weights):
+    """
+     Takes an input vector and runs it through a 1-layer neural network
+     with a given weight matrix and returns the output.
+
+     Arg:
+       inputs - 2 x 1 NumPy array
+       weights - 2 x 1 NumPy array
+     Returns (in this order):
+       out - a 1 x 1 NumPy array, representing the output of the neural network
+    """
+    return np.tanh(np.dot(inputs,weights))
+    raise NotImplementedError
+
+def scalar_function(x, y):
+    """
+    Returns the f(x,y) defined in the problem statement.
+    """
+    if x <= y:
+        return x*y
+    else:
+        return x/y
+    raise NotImplementedError
+
+def vector_function(x, y):
+    """
+    Make sure vector_function can deal with vector input x,y
+    """
+    fvector = np.vectorize(scalar_function)
+    fvector(x,y)
+    raise NotImplementedError
